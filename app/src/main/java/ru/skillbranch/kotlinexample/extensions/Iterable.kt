f8a441f156dc059654fa6f7532a3ce6012cc366f
Lobotino <Lobotino1@gmail.com>
2021-03-28 17:08:54 +0500

    iterable to fun

--- app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
@@ -1,19 +1,16 @@
 package ru.skillbranch.kotlinexample.extensions
 
-class Iterable {
-
-    fun<T> List<T>.dropLastUntil(predicate: (T) -> Boolean) : List<T>{
-        val mutableResult = this.toMutableList()
-        if (!isEmpty()) {
-            for(i in size-1 downTo 0) {
-                if(!predicate(this[i])) {
-                    mutableResult.removeAt(i)
-                } else {
-                    mutableResult.removeAt(i)
-                    return mutableResult
-                }
+fun<T> List<T>.dropLastUntil(predicate: (T) -> Boolean) : List<T> {
+    val mutableResult = this.toMutableList()
+    if (!isEmpty()) {
+        for (i in size - 1 downTo 0) {
+            if (!predicate(this[i])) {
+                mutableResult.removeAt(i)
+            } else {
+                mutableResult.removeAt(i)
+                return mutableResult
             }
         }
-        return emptyList()
     }
+    return emptyList()
 }
